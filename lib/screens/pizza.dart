d6e54bf4dcac03a18111980045d1d214f9ce288d
BB Caspian <casfian@gmail.com>
2022-02-08 10:03:34 +0800

    change appbar color

--- lib/screens/pizza.dart
+++ lib/screens/pizza.dart
@@ -10,6 +10,7 @@ class Pizza extends StatelessWidget {
           iconTheme: const IconThemeData(color: Colors.black),
           title: const Text('Pizza', style: TextStyle(color: Colors.black),),
           backgroundColor: const Color.fromRGBO(245, 245, 245, 1.0),
+          elevation: 0,
         ),
         body: Center(
           child: Container(
