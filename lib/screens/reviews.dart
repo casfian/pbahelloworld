ecc17607b4ce066eaf79491438e8b42e10461270
BB Caspian <casfian@gmail.com>
2022-02-08 16:18:11 +0800

    added futurebuilder

--- lib/screens/reviews.dart
+++ lib/screens/reviews.dart
@@ -1,27 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Reviews extends StatelessWidget {
+class Reviews extends StatefulWidget {
   const Reviews({Key? key}) : super(key: key);
 
+  @override
+  State<Reviews> createState() => _ReviewsState();
+}
+
+class _ReviewsState extends State<Reviews> {
+  var data;
+
+  //buat function
+  getReview() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await http.get(url);
+    //print('Response status: ${response.statusCode}');
+    //print('Response body: ${response.body}');
+
+    //kita hanya nak data yg dlm response body
+    //l=kita convert to json supaya senang nk parsing nanti
+    data = json.decode(response.body);
+
+    return data;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //getReview();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          iconTheme: const IconThemeData(color: Colors.black),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.black),
         title: const Text(
           'Reviews',
           style: TextStyle(color: Colors.black),
         ),
         backgroundColor: const Color.fromRGBO(245, 245, 245, 1.0),
         elevation: 0,
-        ),
-        body: Center(
-          child: Container(
-            child: const Text('Reviews'),
-          ),
-        
+        actions: [
+          IconButton(
+              onPressed: () {
+                //add review
+              },
+              icon: const Icon(Icons.add)),
+        ],
       ),
+      body: FutureBuilder(
+          future: getReview(),
+          builder: (context, AsyncSnapshot snapshot) {
+            print(snapshot.data);
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ListView.builder(
+                  itemCount: snapshot.data == null ? 0 : snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                          title: Text(
+                            snapshot.data[index]['title'],
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                          subtitle: Text(snapshot.data[index]['body'])),
+                    );
+                  });
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 }
+
+
+/*
+
+*/
