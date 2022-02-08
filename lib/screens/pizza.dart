0dd59fcda6226f14974bf4ffe6034a9bccd707bb
BB Caspian <casfian@gmail.com>
2022-02-08 10:12:45 +0800

    change appbars

--- lib/screens/pizza.dart
+++ lib/screens/pizza.dart
@@ -12,12 +12,28 @@ class Pizza extends StatelessWidget {
           backgroundColor: const Color.fromRGBO(245, 245, 245, 1.0),
           elevation: 0,
         ),
-        body: Center(
-          child: Container(
-            child: const Text('Pizza'),
-          ),
+        body: ListView(
+          children: [
+            ListTile(
+              leading: Image.asset('assets/images/pizza1.png'),
+              title: Text('Pizza 1: Chicken Pizza'),
+              subtitle: Text('Harga RM12:00'),
+              trailing: Text('Buy NOW'),
+            ),
+            ListTile(
+              leading: Image.asset('assets/images/pizza2.png'),
+              title: Text('Pizza 2: Hawaiian Pizza'),
+              subtitle: Text('Harga RM14:00'),
+              trailing: Text('Buy NOW'),
+            ),
+            ListTile(
+              leading: Image.asset('assets/images/pizza3.png'),
+              title: Text('Pizza 3: Hawaiian Pizza'),
+              subtitle: Text('Harga RM16:00'),
+              trailing: Text('Buy NOW'),
+            ),
+          ],
         ),
-      
     );
   }
 }
